1f514d4fda0a916740a578bbe636844644bc54a6
Aiganish <you@example.com>
2023-08-07 18:20:26 +0300

    first commit

---
+++ bin/dart_built_types_home_work.dart
@@ -0,0 +1,81 @@
+void main(List<String> arguments) {
+  String str06 = 'Kyrgyzcha Flutter15';
+  print(str06);
+
+  num num05 = 10;
+  print(num05);
+
+  int int04 = 28;
+  print(int04);
+
+  double double03 = 8.0;
+  print(double03);
+
+  bool bool02 = true;
+  print(bool02);
+
+  print('$str06,$num05,$int04,$double03,$bool02');
+
+  print('\n$str06,\n$num05,\n$int04,\n$double03,\n$bool02');
+
+  // String example.
+  String firstName = 'Alymbek';
+  String lastName = 'Uraanbek';
+  print('Menin atym $firstName,atamdyn aty $lastName');
+
+  //Num example.
+  num san1 = 49;
+  num san2 = 36;
+  print('$san1+$san2=${san1 + san2}');
+
+  //Int example.
+  int san5 = 90;
+  int san6 = 80;
+  print('$san5+$san6=${san5 + san6}');
+
+  //Bool example.
+  bool correct = true;
+  bool unCorrect = false;
+  print('1+1=2 $correct');
+  print('1+3=2 $unCorrect');
+
+  //Dynamic.
+  dynamic dynamic01 = 'Aiganish';
+  print(dynamic01);
+}
+
+void main02(List<String> args) {
+  // 1) formula.
+  // Type variable-name = variable-value.
+  String name1 = 'Atabek';
+  int jash2 = 90;
+
+  // 2) formula.
+  // var variable-name = varible-value.
+  var name2 = 'flutter';
+  var jash1 = 90;
+
+  //Int example.
+  int san5 = 90;
+  int san6 = 80;
+  print('$san5+$san6=${san5 + san6}');
+
+  //Double example.
+  double san4 = 40;
+  double san3 = 28;
+  print('$san4+$san3=${san4 + san3}');
+}
+
+void main03(List<String> args) {
+  // 1: 1 {function} je bolboso bir {class} ichinde okshosh attagy ozgorulmo tuzo albaibyz.
+  var name = 'Name';
+  String name2 = 'Name';
+
+  // 2: birinchi soz kichine tamga menen kiyinki sozdor chon menen ozgorulmonun attary jazylat.
+  num userCount = 1234;
+
+  // 3:Type berilgen variable kiyin type ozgorboit.
+  int mount = 1234;
+  mount = 4321;
+  //mount = 'Mount';
+}
---
+++ test/dart_built_types_home_work_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_built_types_home_work/dart_built_types_home_work.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
